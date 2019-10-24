baa2edb58cf49583fd6aa01f3f1c68c9c5f6e5a9
Joseph Driscoll <driscoll.recording@gmail.com>
2019-10-24 09:51:24 -0500

    Update Resources.Designer.cs

--- TrackerUI/Properties/Resources.Designer.cs
+++ TrackerUI/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TracterUI.Properties
+namespace TrackerUI.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace TracterUI.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TracterUI.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TrackerUI.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
